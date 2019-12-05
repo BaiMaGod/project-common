236780f51f29290e9c11892d40bf0dde3b6ed36d
BaiMaGod <749865308@qq.com>
2019-12-05 20:57:20 +0800

    增加模块模板自动转化 功能

--- src/main/java/com/ModuleGenerator.java
+++ src/main/java/com/ModuleGenerator.java
@@ -2,6 +2,10 @@ package com;
 
 public class ModuleGenerator {
     public static void main(String[] args) {
-        ModuleCreator.create();
+        // 生成 controller、form、service 代码
+//        ModuleCreator.create();
+
+        // 将当前项目转为模板
+        ModuleCreator.convert();
     }
 }
